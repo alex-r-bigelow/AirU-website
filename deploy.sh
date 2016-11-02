f88ddc88a148b278aaad46cb6ba6a3e911d3469a
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-11-02 14:38:25 -0600

    Fix mongo setup

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,6 @@
 WORKING_DIR=${1:-`pwd`}
 echo "*** Using $WORKING_DIR as working directory"
 
-echo "Choose a username for the REST admin interface:"
-read ADMIN_USERNAME
-echo "Set a password:"
-
 echo "*** Installing the basics..."
 apt-get install -y curl g++ git libssl-dev libffi-dev make python-dev python-pip libjpeg-dev zlib1g-dev
 apt-get install -y python-pip python-dev build-essential
@@ -19,14 +15,15 @@ echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' |
 apt-get update
 apt-get install -y mongodb-org-shell mongodb-org-server
 
-if [ -z $1 ] ; then
+if [ $1 = "/vagrant" ] ; then
   # Only apply our custom mongo.conf for vagrant installs (it allows
   # connections from outside the VM, but we want the more secure settings in
   # deployment)
+  echo "*** Allow foreign mongo connections"
   cp $WORKING_DIR/config/mongod.conf /etc/mongod.conf
 fi
 cp $WORKING_DIR/config/mongod.service /lib/systemd/system/mongod.service
-service mongod restart
+systemctl enable mongod
 
 echo "*** Installing needed libraries for REST server..."
 pip install eve --upgrade
