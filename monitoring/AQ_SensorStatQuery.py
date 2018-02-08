44a45c5e8c601cae4fb4553b7da520cd755ed6b9
Pascal Goffin <ppjgoffin@gmail.com>
2018-02-07 21:23:09 -0700

    output

--- monitoring/AQ_SensorStatQuery.py
+++ monitoring/AQ_SensorStatQuery.py
@@ -105,7 +105,7 @@ def runMonitoring(config, timeFrame, isSchool, borderBox, pAirClient, airUClient
     # Printing the status of the sensors in the required box
     theMessage = ''
     theMessage = theMessage + '            \t             \t          \t          \t             \t        Query Status         \t             \t             \n'
-    theMessage = theMessage + 'ID          \tSensor Holder\temail     \tLatitude   \tLongitude    \toffline/failure/online (total)\tLatest Status \tLast Data Value      \n'
+    theMessage = theMessage + 'ID          \tSensor Holder\temail                      \tLatitude   \tLongitude    \toffline/failure/online (total)\tLatest Status \tLast Data Value      \n'
     theMessage = theMessage + '------------\t----------------------------\t-----------\t-----------\t------------\t------------------------------\t--------------\t--------------\n'
 
     for anID in tmpIDs:
