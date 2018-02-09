7ffd139ac366c6992f9f88231c56eaf0666c264d
Pascal Goffin <ppjgoffin@gmail.com>
2018-02-09 10:43:47 -0700

    removed matplotlib calls

--- modeling/gradDescent.py
+++ modeling/gradDescent.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as pl
+# import matplotlib.pyplot as pl
 
 def gradDescent(gradFun,Fun,theta0,optList,tol,gamma0,maxIt,plotObj):
 # Finds the theta values that minimizes the objective function Fun
-# 
+#
 # gradFun: a function that calculates gradients of the objective functions with respect to theta and stores it in a 1D numpy matrix
-# Fun: a function that calculates the objective 
+# Fun: a function that calculates the objective
 # theta0: initial values for theta
 # optList: a boolean list that defines which dimensions of theta to be optimized
 # tol: the convergence tolerance
@@ -20,14 +20,14 @@ def gradDescent(gradFun,Fun,theta0,optList,tol,gamma0,maxIt,plotObj):
     obj = Fun(theta0)
     print 'Objective = ' + str(obj)
 
-    if plotObj:
-#        pl.ion()
-        pl.xlabel('#iterations')
-        pl.ylabel('objective value')
-        pl.plot(it,obj,'bo',markersize=8)
-        pl.pause(0.05)
-#        fig.canvas.draw()
-#        pl.show()
+#     if plotObj:
+# #        pl.ion()
+#         pl.xlabel('#iterations')
+#         pl.ylabel('objective value')
+#         pl.plot(it,obj,'bo',markersize=8)
+#         pl.pause(0.05)
+# #        fig.canvas.draw()
+# #        pl.show()
 
 
     while err>tol and it<=maxIt:
@@ -38,12 +38,12 @@ def gradDescent(gradFun,Fun,theta0,optList,tol,gamma0,maxIt,plotObj):
         # theta[1] = theta[1] - gamma * grad[1]
         theta = theta - gamma * np.select([optList], [grad.T])
         obj_n = Fun(theta.tolist()[0])
-        if plotObj:
-            pl.plot(it,obj_n,'bo',markersize=8)
-            pl.pause(0.05)
-#            fig.canvas.draw()
+#         if plotObj:
+#             pl.plot(it,obj_n,'bo',markersize=8)
+#             pl.pause(0.05)
+# #            fig.canvas.draw()
 
-        
+
         err = abs(obj-obj_n)
         obj = obj_n
         print 'it #'+str(it)+ ': Objective change = ' + str(err)
