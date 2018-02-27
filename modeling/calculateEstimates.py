ba4c35b62ad9a03b1aeff47324e52ee7230953c6
Pascal Goffin <ppjgoffin@gmail.com>
2018-02-27 09:52:58 -0700

    to bytes

--- modeling/calculateEstimates.py
+++ modeling/calculateEstimates.py
@@ -169,7 +169,7 @@ def calculateContours(X, Y, Z):
     theSVG = '<svg' + theSVG.split('<svg')[1]
     stringFile.close()
     print(theSVG)
-    binaryFile = Binary(BytesIO(stringFile))
+    binaryFile = Binary(str.encode(stringFile))
     return binaryFile
 
 
