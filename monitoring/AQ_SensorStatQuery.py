4aba8edd6ccda24c2580bbccd08547c06cbdc5e6
Pascal Goffin <ppjgoffin@gmail.com>
2018-03-01 22:54:35 -0700

    exchanged open with io.open

--- monitoring/AQ_SensorStatQuery.py
+++ monitoring/AQ_SensorStatQuery.py
@@ -1,4 +1,5 @@
 import csv
+import io
 import json
 import logging
 import logging.handlers as handlers
@@ -30,7 +31,7 @@ def getConfig():
 
 def appendToCSVFile(fileName, data):
 
-    with open(fileName, 'a', newline='\n') as csvFile:
+    with io.open(fileName, 'a', newline='\n') as csvFile:
         writer = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_ALL)
         writer.writerow(data)
 
