6cdb0df7c7e1d1e998ade95f9f80d4440d8a909d
Pascal Goffin <ppjgoffin@gmail.com>
2018-04-25 16:27:21 -0600

    debug

--- poll/pollingDAQ.py
+++ poll/pollingDAQ.py
@@ -14,12 +14,12 @@ from influxdb import InfluxDBClient
 TIMESTAMP = datetime.now().isoformat()
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.INFO)
+LOGGER.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - [%(funcName)s:%(lineno)d] - %(levelname)s - %(message)s')
 
 logHandler = handlers.RotatingFileHandler('daqPoller.log', maxBytes=5000000, backupCount=5)
-logHandler.setLevel(logging.INFO)
+logHandler.setLevel(logging.DEBUG)
 logHandler.setFormatter(formatter)
 LOGGER.addHandler(logHandler)
 
