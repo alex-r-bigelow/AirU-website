c87e1c56b91c9788d468ab50b32435f02426c9c0
Pascal Goffin <ppjgoffin@gmail.com>
2018-07-26 16:47:38 -0600

    fix

--- modeling/reRunEstimationCalculation.py
+++ modeling/reRunEstimationCalculation.py
@@ -17,7 +17,7 @@ def main(args):
     interval = timedelta(seconds=args.interval)
 
     while startQuerytime < endQuerytime:
-        calculateEstimates.main(['false', '--d', 'modellingConfig_debugging.json', '-q', '2018-07-26T16:00:00Z'])
+        calculateEstimates.main(['false', '--d', 'modellingConfig_debugging.json', '-q', startQuerytime.stftime('%Y-%m-%dT%H:%M:%SZ')])
         startQuerytime += interval
 
 
