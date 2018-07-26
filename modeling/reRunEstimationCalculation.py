cf7df629968d7ec78bfe488b622603f341862b65
Pascal Goffin <ppjgoffin@gmail.com>
2018-07-26 16:35:35 -0600

    script to rerun the estimates

---
+++ modeling/reRunEstimationCalculation.py
@@ -0,0 +1,25 @@
+import argparse
+import calculateEstimates
+import sys
+from datetime import datetime, timedelta
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("startQuerytime", help="start query time (UTC) for estimation with format: %Y-%m-%dT%H:%M:%SZ")
+    parser.add_argument("endQuerytime", help="end query time (UTC) for estimation with format: %Y-%m-%dT%H:%M:%SZ")
+    parser.add_argument("interval", help="interval until next estimate calculation in seconds")
+
+    args = parser.parse_args()
+
+    startQuerytime = datetime.strptime(args.startQuerytime, '%Y-%m-%dT%H:%M:%SZ')
+    endQuerytime = datetime.strptime(args.endQuerytime, '%Y-%m-%dT%H:%M:%SZ')
+    interval = timedelta(seconds=args.interval)
+
+    while startQuerytime < endQuerytime:
+        calculateEstimates.main(['false', '--d', 'modellingConfig_debugging.json', '-q', '2018-07-26T16:00:00'])
+        startQuerytime += interval
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
