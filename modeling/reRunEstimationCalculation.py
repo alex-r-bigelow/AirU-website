bd7564f2d588594822f5738730e3202c3ee08b50
Pascal Goffin <ppjgoffin@gmail.com>
2018-08-02 21:49:54 -0600

    adding timeing and logging info

--- modeling/reRunEstimationCalculation.py
+++ modeling/reRunEstimationCalculation.py
@@ -1,7 +1,21 @@
 import argparse
 import calculateEstimates
+import logging
+import logging.handlers as handlers
 import sys
+import time
 from datetime import datetime, timedelta
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - [%(funcName)s:%(lineno)d] - %(levelname)s - %(message)s')
+
+# logHandler = handlers.TimedRotatingFileHandler('cronPMEstimation.log', when='D', interval=1, backupCount=3)
+logHandler = handlers.RotatingFileHandler('reRunEstimation.log', maxBytes=5000000, backupCount=5)
+logHandler.setLevel(logging.INFO)
+logHandler.setFormatter(formatter)
+LOGGER.addHandler(logHandler)
 
 
 def main(args):
@@ -17,8 +31,17 @@ def main(args):
     interval = timedelta(seconds=int(args.interval))
 
     while startQuerytime < endQuerytime:
+        LOGGER.info('START timstep: %s', startQuerytime.strftime('%Y-%m-%dT%H:%M:%SZ'))
+
+        start = time.time()
+
         calculateEstimates.main(['false', '--d', 'modellingConfig_debugging.json', '-q', startQuerytime.strftime('%Y-%m-%dT%H:%M:%SZ')])
         startQuerytime += interval
+
+        end = time.time()
+
+        LOGGER.info('*********** Time to calculate estimate: {}', format(end - start))
+        LOGGER.info('Finished timestep: %s', startQuerytime.strftime('%Y-%m-%dT%H:%M:%SZ'))
 
 
 if __name__ == '__main__':
